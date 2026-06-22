652a5933b739cbc56ba15e825724bf4d26c4df1e
Agustin Moreno <agustinmooreno07@gmail.com>
2026-06-22 14:04:23 -0300

    Update juego4.py

--- juego4.py
+++ juego4.py
@@ -1,12 +1,61 @@
 import random
 
+TAM = 5
+BARCOS = 5
+
+
+# Crear tablero
 def crear_tablero():
-    matriz = [[0 for j in range(5)] for i in range(5)]
+
+    matriz = [[0 for j in range(TAM)] for i in range(TAM)]
+
     return matriz
 
-def quedan_barcos(matriz):
-    for i in range(5):
-        for j in range(5):
-            if matriz[i][j] == 1:
+
+# Quedan barcos
+def quedan_barcos(tablero):
+
+    for i in range(TAM):
+        for j in range(TAM):
+
+            if tablero[i][j] == 1:
                 return True
+
     return False
+
+
+# Ganador
+def ganador(tablero_jugador, tablero_ia):
+
+    if not quedan_barcos(tablero_ia):
+        return "Jugador"
+
+    if not quedan_barcos(tablero_jugador):
+        return "IA"
+
+    return None
+
+
+# Colocar barcos IA
+def colocar_barcos_ia(tablero):
+
+    barcos_colocados = 0
+
+    while barcos_colocados < BARCOS:
+
+        fila = random.randint(0, TAM - 1)
+        columna = random.randint(0, TAM - 1)
+
+        if tablero[fila][columna] == 0:
+
+            tablero[fila][columna] = 1
+            barcos_colocados += 1
+
+
+tablero_jugador = crear_tablero()
+tablero_ia = crear_tablero()
+
+
+colocar_barcos_ia(tablero_ia)
+
+
