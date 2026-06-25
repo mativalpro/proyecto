16f30f7268cf0257f17ebfd800bfd920b07225ed
Agustin Moreno <agustinmooreno07@gmail.com>
2026-06-25 13:58:14 -0300

    Update juego4.py

--- juego4.py
+++ juego4.py
@@ -68,3 +68,57 @@ tablero_ia_disparos = crear_tablero()
 
 colocar_barcos_ia(tablero_ia_real)
 colocar_barcos_jugador(tablero_jugador)
+
+print("\n¡Empieza la batalla!")
+
+while quedan_barcos(tablero_jugador) and quedan_barcos(tablero_ia_real):
+    mostrar_tableros(tablero_jugador, tablero_ia_disparos)
+
+    print("Tu turno de disparo ")
+    tiro_valido = False
+    while not tiro_valido:
+        try:
+            fila = int(input("Fila a disparar (0-4): "))
+            columna = int(input("Columna a disparar (0-4): "))
+
+            if 0 <= fila < TAM and 0 <= columna < TAM:
+                if tablero_ia_disparos[fila][columna] in (2, 3):
+                    print("Ya disparaste ahí. Intenta otra casilla.")
+                    continue
+
+                if tablero_ia_real[fila][columna] == 1:
+                    print("impacto")
+                    tablero_ia_real[fila][columna] = 3
+                    tablero_ia_disparos[fila][columna] = 3
+                else:
+                    print("Agua")
+                    tablero_ia_disparos[fila][columna] = 2
+                tiro_valido = True
+            else:
+                print("Coordenadas inválidas.")
+        except ValueError:
+            print("Introduce un numero valido")
+
+    if not quedan_barcos(tablero_ia_real):
+        break
+
+    print("\nTurno IA")
+    ia_tiro_valido = False
+    while not ia_tiro_valido:
+        fila_ia = random.randint(0, TAM - 1)
+        columna_ia = random.randint(0, TAM - 1)
+
+        if tablero_jugador[fila_ia][columna_ia] in (0, 1):
+            if tablero_jugador[fila_ia][columna_ia] == 1:
+                print(f"IA Disparo en ({fila_ia},{columna_ia}) Impacto")
+                tablero_jugador[fila_ia][columna_ia] = 4
+            else:
+                print(f"IA Disparo en ({fila_ia},{columna_ia}) Agua")
+                tablero_jugador[fila_ia][columna_ia] = 2
+            ia_tiro_valido = True
+
+mostrar_tableros(tablero_jugador, tablero_ia_disparos)
+if not quedan_barcos(tablero_ia_real):
+    print("¡Ganaste la partida!")
+else:
+    print("La IA hundió todos tus barcos.")
